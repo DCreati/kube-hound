5e5f719a285292e28a8c238e5a71f827eebdb10c
DCreati <d.creati@campus.unimib.it>
2023-05-17 12:18:10 +0200

    Correct docker usage in analisys
    Nell'analisi con kubsec.io è stato rimosso il container docker e sostituito con una richiesta http diretta al servizio

--- kube_hound/builtin_analyses/unnecessary_privileges_pods.py
+++ kube_hound/builtin_analyses/unnecessary_privileges_pods.py
@@ -1,12 +1,13 @@
-from time import sleep
-from typing import List, Mapping
-from kube_hound.analysis import AnalysisResult, StaticAnalysis
-from loguru import logger
 import json
 import docker
 import requests
+import socket
 
+from time import sleep
+from typing import List, Mapping
+from loguru import logger
 
+from kube_hound.analysis import AnalysisResult, StaticAnalysis
 from kube_hound.applicationobject import ApplicationObject
 from kube_hound.smells import Smell
 
@@ -19,38 +20,25 @@ class UnnecessaryPrivilegesToPods(StaticAnalysis):
 
     def run_analysis(self, input_objects: Mapping[str, List[ApplicationObject]]) \
             -> List[AnalysisResult]:
+        
         kubernetes_objects = input_objects.get('kubernetes_config')
         if kubernetes_objects is None:
             return []
 
-        # retrieve the docker client
-        self.docker_client = docker.from_env()
-
-        # spawn a kubesec container
-        logger.debug('spawning kubesec container')
-        kubesec_container = self.docker_client.containers.run(
-            'kubesec/kubesec:v2', detach=True, ports={'8080/tcp': 8080})
-
         output_results = []
-        try:
-            self.wait_for_running_container(kubesec_container)
 
-            # for each different Kubernetes config, send it to Kubesec
-            # and join the results
-            seen_paths = set()
-            for obj in kubernetes_objects:
-                if str(obj.path) in seen_paths:
-                    continue
-                seen_paths.add(str(obj.path))
+        # for each different Kubernetes config, send it to Kubesec
+        # and join the results
+        seen_paths = set()
+        for obj in kubernetes_objects:
+            if str(obj.path) in seen_paths:
+                continue
+            seen_paths.add(str(obj.path))
 
-                logger.debug(f"processing kubernetes config {obj.path.name}")
+            logger.debug(f"processing kubernetes config {obj.path.name}")
 
-                results = self.__process_kubernetes_config(obj)
-                output_results += results
-        finally:
-            # cleanup Kubesec container
-            kubesec_container.stop()
-            kubesec_container.remove()
+            results = self.__process_kubernetes_config(obj)
+            output_results += results
 
         return output_results
 
@@ -65,7 +53,7 @@ class UnnecessaryPrivilegesToPods(StaticAnalysis):
 
         # send the content to kubesec
         response = requests.post(
-            'http://localhost:8080/scan', data=raw_data)
+            'https://v2.kubesec.io/scan', data=raw_data)
 
         # parse the results
         data = json.loads(response.text)
@@ -88,18 +76,8 @@ class UnnecessaryPrivilegesToPods(StaticAnalysis):
                 results.append(
                     AnalysisResult(description, {Smell.UPM}))
         return results
-
-    def wait_for_running_container(self, container):
-        """
-        Actively wait for a container to become running
-        """
-        timeout = 120
-        stop_time = 0.5
-        elapsed_time = 0
-
-        while elapsed_time < timeout:
-            sleep(stop_time)
-            elapsed_time += stop_time
-            container.reload()
-            if container.status == 'running':
-                return
+    
+    def __get_free_port(self):
+        sock = socket.socket()
+        sock.bind(('', 0))
+        return sock.getsockname()[1]
